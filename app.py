57a5b5efdb1d7aea780a82b86fdacf90e3b28870
RefOoSami001 <159632003+RefOoSami001@users.noreply.github.com>
2025-01-31 03:08:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,6 @@ def get_grades():
 
                 name_req = requests.post('http://stda.minia.edu.eg/PortalgetJCI', cookies=cookies, headers=headers, data=data, verify=False).json()
                 full_name = name_req[0]['Name'].split('|')[0]
-
                 for entry in response_json:
                     try:
                         year = entry['ScopeName'].split('-')[0].strip()
@@ -114,30 +113,31 @@ def get_grades():
                             year_category = "Fourth"
                         else:
                             continue
+                        print(entry)
+                        if entry.get('ds') and len(entry['ds']) > 0:
+                            for course in entry['ds'][0].get('StudyYearCourses', []):
+                                course_name = course['CourseName'].replace("||", '')
+                                max_score = course['Max']
+                                total_score = course['Total']
+                                grade_name = course["GradeName"].split("|")[0]
 
-                        for course in entry['ds'][0]['StudyYearCourses']:
-                            course_name = course['CourseName'].replace("||", '')
-                            max_score = course['Max']
-                            total_score = course['Total']
-                            grade_name = course["GradeName"].split("|")[0]
+                                if total_score:
+                                    percentage = (float(total_score) / float(max_score)) * 100
+                                else:
+                                    percentage = 0
+                                    total_score = 0
+                                    grade_name = "غير معروف"
 
-                            if total_score:
-                                percentage = (float(total_score) / float(max_score)) * 100
-                            else:
-                                percentage = 0
-                                total_score = 0
-                                grade_name = "غير معروف"
+                                max_degrees += float(max_score)
+                                total_degrees += float(total_score)
 
-                            max_degrees += float(max_score)
-                            total_degrees += float(total_score)
-
-                            grades_by_year[year_category].append({
-                                'course_name': course_name,
-                                'grade': grade_name,
-                                'max_score': max_score,
-                                'total_score': total_score,
-                                'percentage': int(percentage)
-                            })
+                                grades_by_year[year_category].append({
+                                    'course_name': course_name,
+                                    'grade': grade_name,
+                                    'max_score': max_score,
+                                    'total_score': total_score,
+                                    'percentage': int(percentage)
+                                })
                     except:
                         flash("لم تظهر النتيجة بعد حاول مرة اخري لاحقا!")
                         return redirect(url_for('index'))
@@ -181,5 +181,6 @@ def get_grades():
     except Exception as e:
         flash(f"حدث خطأ غير متوقع: {str(e)}")
         return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.run(debug=True)
